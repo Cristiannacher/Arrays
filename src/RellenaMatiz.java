d6b0b817893e1c0031eb1e528f8996bda05e8128
Cristian <alunac6050@ieselcaminas.org>
2021-11-30 17:43:43 +0100

    Comit 9

---
+++ src/RellenaMatiz.java
@@ -0,0 +1,13 @@
+public class RellenaMatiz {
+
+    public static int[][] rellenaMatriz(int[][] numeros){
+        for(int i = 0; i< numeros.length; i++){
+            for(int j = 0; j< numeros[0].length; j++){
+                numeros[i][j] =
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
